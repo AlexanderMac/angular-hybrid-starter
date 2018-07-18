7665a750fc512c80f03985d12fd3b0742d78ae95
AlexanderMac <amatsibarov@gmail.com>
2018-07-19 02:08:41 +0300

    add -ajx postfix for Angular.js feature modules

--- src/app/app-ajs.ts
+++ src/app/app-ajs.ts
@@ -1,11 +1,11 @@
 import * as ng from 'angular';
 
-import './_core/module';
-import './_shared/module';
-import './home/module';
-import './roles/module';
-import './users/module';
-import './test/module.ajs';
+import './_core/module-ajs';
+import './_shared/module-ajs';
+import './home/module-ajs';
+import './roles/module-ajs';
+import './users/module-ajs';
+import './test/module-ajs';
 
 import './app-ajs.styl';
 
