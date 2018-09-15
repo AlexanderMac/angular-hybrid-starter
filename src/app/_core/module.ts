208684bec42abd310125ccfe03e32b2d7527ae98
AlexanderMac <amatsibarov@gmail.com>
2018-09-16 00:46:06 +0300

    create route-params angular serviceProvider

--- src/app/_core/module.ts
+++ src/app/_core/module.ts
@@ -1,9 +1,11 @@
 import { NgModule }                               from '@angular/core';
+import { routeParamsProvider }                    from './route-params.service';
 import { localStorageRepoServiceFactoryProvider } from './localstorage-repo.service';
 import { notificationServiceProvider }            from './notification.service';
 
 @NgModule({
   providers: [
+    routeParamsProvider,
     localStorageRepoServiceFactoryProvider,
     notificationServiceProvider
   ]
